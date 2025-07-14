c5a72b225396d053ae9322f84cfbd3095f31e9ba
Harsh <kumarharsh93982@gmail.com>
2025-07-14 21:05:54 +0530

    Bug resolve

--- Week_01/Day_07/printTheMatrixInSpiralManner.cpp
+++ Week_01/Day_07/printTheMatrixInSpiralManner.cpp
@@ -9,7 +9,41 @@ vector<int> spiralOrder(vector<vector<int>>& matrix)
     int col = matrix[0].size();
 
     vector<int> res;
+     // Initialize the pointers reqd for traversal.
+  int top = 0, left = 0, bottom = row - 1, right = col - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+      
+    // For moving left to right
+    for (int i = left; i <= right; i++)
+      res.push_back(matrix[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (int i = top; i <= bottom; i++)
+      res.push_back(matrix[i][right]);
+
+    right--;
     
+    // For moving right to left.
+    if (top <= bottom) {
+      for (int i = right; i >= left; i--)
+       res.push_back(matrix[bottom][i]);
+
+      bottom--;
+    }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (int i = bottom; i >= top; i--)
+        res.push_back(matrix[i][left]);
+
+      left++;
+    }
+  }
+  return res;
 
     return res;
     
