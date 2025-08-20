4e1c40c0889cb214e1f97e53ee719079962d88cc
Harsh <kumarharsh93982@gmail.com>
2025-08-20 13:44:56 +0530

    Print N To 1 Using Recursion.

---
+++ Week_07/Day_03/print_N_To_1_UsingRecursion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void printNumbers(int n) 
+{
+    if(n == 0)return;
+
+    cout<<n<<endl;
+    printNumbers(n-1);
+}
+
+int main()
+{
+    int n = 5;
+    printNumbers(n);
+
+    return 0;
+}
