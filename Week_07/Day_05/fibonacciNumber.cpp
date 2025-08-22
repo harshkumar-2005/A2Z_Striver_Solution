3135c6ed4dd0ee4f696fe8e7ae6157215bb7e4d7
Harsh <kumarharsh93982@gmail.com>
2025-08-22 23:14:54 +0530

    Fibonacci Number.

---
+++ Week_07/Day_05/fibonacciNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int fib(int n) 
+{
+    if(n == 0 || n == 1) return n;
+
+    return fib(n-1) + fib(n-2);
+    
+}
+
+int main()
+{
+    int n = 10;
+    int res = fib(n);
+    cout<< res;
+
+    return 0;
+}
